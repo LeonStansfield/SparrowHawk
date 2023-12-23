25a683a2042bddc7b57285b40bdaa6d34cd8e07b
LeonStansfield <leonstansfield208@gmail.com>
2023-12-23 17:22:11 +0000

    Basic dynamic scene loading

--- Main.cpp
+++ Main.cpp
@@ -13,52 +13,87 @@ public:
     SceneManager() {
     }
 
-    void loadScene(std::string sceneName) {
+    std::vector<std::vector<std::string>> loadScene(std::string sceneName) {
         // Open the scene file
         std::ifstream sceneFile(sceneName);
         if (!sceneFile.is_open()) {
             std::cerr << "Failed to open scene file: " << sceneName << std::endl;
-            return;
+            return {};
         }
 
-        // Parse scene file and create objects
+        std::cout << "Loading scene: " << sceneName << std::endl;
+
+        std::string name = "";
+        Vector3 location = { 0.0f, 0.0f, 0.0f };
+        Vector3 rotation = { 0.0f, 0.0f, 0.0f };
+        Vector3 scale = { 1.0f, 1.0f, 1.0f };
+        std::string parent = "";
+
+        std::vector<std::vector<std::string>> objectData;
         std::string line;
         while (std::getline(sceneFile, line)) {
-            std::istringstream iss(line);
-            std::string token;
-            iss >> token;
+            // Skip empty lines
+            if (line.empty()) {
+				continue;
+			}
+            // If line starts with 'Object'
+            if (line.find("Object") == 0) {
+                // Set the name to the second word in the line
+                name = line.substr(line.find(" ") + 1);
+                // Remove space from start of name
+                name.erase(0, name.find_first_not_of(" "));
+			}
+            // If line starts with 'Location'
+            else if (line.find("Location") == 0) {
+				// Set the location to the values between the brackets
+                float x = std::stof(line.substr(line.find("(") + 1, line.find(",") - line.find("(") - 1));
+                float y = std::stof(line.substr(line.find(",") + 1, line.find(")") - line.find(",") - 1));
+                float z = std::stof(line.substr(line.find_last_of(" ") + 1));
+                location = Vector3{ x, y, z };
+            }
+            // If line starts with 'Rotation'
+            else if (line.find("Rotation") == 0) {
+                // Set the rotation to the values between the brackets
+                float x = std::stof(line.substr(line.find("(") + 1, line.find(",") - line.find("(") - 1));
+                float y = std::stof(line.substr(line.find(",") + 1, line.find(")") - line.find(",") - 1));
+                float z = std::stof(line.substr(line.find_last_of(" ") + 1));
+                rotation = Vector3{ x, y, z };
+            }
+            // If line starts with 'Scale'
+            else if (line.find("Scale") == 0) {
+                // Set the scale to the values between the brackets
+                float x = std::stof(line.substr(line.find("(") + 1, line.find(",") - line.find("(") - 1));
+                float y = std::stof(line.substr(line.find(",") + 1, line.find(")") - line.find(",") - 1));
+                float z = std::stof(line.substr(line.find_last_of(" ") + 1));
+                scale = Vector3{ x, y, z };
+            }
+            // If line starts with 'Parent'
+            else if (line.find("Parent") == 0) {
+                // Set the parent to the second word in the line
+                parent = line.substr(line.find(" ") + 1);
+                // Remove space from start of parent
+                parent.erase(0, parent.find_first_not_of(" "));
+            }
+            // If line starts with 'ObjectEnd'
+            else if (line.find("End") == 0) {
+                // All object data is parsed, create object
+                std::cout << name << std::endl;
+                std::cout << location.x << ", " << location.y << ", " << location.z << std::endl;
+                std::cout << rotation.x << ", " << rotation.y << ", " << rotation.z << std::endl;
+                std::cout << scale.x << ", " << scale.y << ", " << scale.z << std::endl;
+                std::cout << parent << std::endl;
 
-            if (token == "Object:") {
-                std::string objName;
-                iss >> objName;
-
-                Vector3 location;
-                iss >> token >> location.x >> location.y >> location.z;
-
-                Vector3 rotation;
-                iss >> token >> rotation.x >> rotation.y >> rotation.z;
-
-                Vector3 scale;
-                iss >> token >> scale.x >> scale.y >> scale.z;
-
-                std::string parentToken;
-                if (iss >> token && token == "Parent:") {
-                    iss >> parentToken;
-                }
-
-                // Print the information for the object
-                std::cout << "Object: " << objName << std::endl;
-                std::cout << "Location: " << location.x << ", " << location.y << ", " << location.z << std::endl;
-                std::cout << "Rotation: " << rotation.x << ", " << rotation.y << ", " << rotation.z << std::endl;
-                std::cout << "Scale: " << scale.x << ", " << scale.y << ", " << scale.z << std::endl;
-                std::cout << "Parent: " << parentToken << std::endl;
+                // Add object data to the vector
+                objectData.push_back({ name, std::to_string(location.x), std::to_string(location.y), std::to_string(location.z),
+                    std::to_string(rotation.x), std::to_string(rotation.y), std::to_string(rotation.z),
+                    std::to_string(scale.x), std::to_string(scale.y), std::to_string(scale.z), parent });
             }
         }
 
         sceneFile.close();
-    }
 
-private:
+        return objectData;
+    }
 };
 
 int main() {
@@ -77,23 +112,27 @@ int main() {
 
     SetTargetFPS(60);
 
-    // Create the scene
-    auto root = std::make_unique<Object>("Root", Vector3{ 0.0f, 0.0f, 0.0f });
-
-    auto floor = std::make_unique<MeshInstance>("Floor", Vector3{ 0.0f, 0.0f, 0.0f }, "Assets/Scenes/Scene_1/Floor.glb");
-    auto Suzanne = std::make_unique<MeshInstance>("Suzanne", Vector3{ 0.0f, 1.5f, 0.0f }, "Assets/Scenes/Scene_1/Suzanne.glb");
-    auto Suzanne_1 = std::make_unique<MeshInstance>("Suzanne_1", Vector3{ 3.0f, 1.5f, 0.0f }, "Assets/Scenes/Scene_1/Suzanne.001.glb");
-    auto Suzanne_2 = std::make_unique<MeshInstance>("Suzanne_2", Vector3{ -3.0f, 1.5f, 0.0f }, "Assets/Scenes/Scene_1/Suzanne.002.glb");
-
-    // Building the tree
-    root->addChild(std::move(floor));
-    root->addChild(std::move(Suzanne));
-    root->addChild(std::move(Suzanne_1));
-    root->addChild(std::move(Suzanne_2));
-
-    // Load the scene
+    // Load the scene data
     SceneManager sceneManager;
-    sceneManager.loadScene("Assets/Scenes/Scene_1/scene.txt");
+    std::vector<std::vector<std::string>> objectData = sceneManager.loadScene("Assets/Scenes/Scene_1/scene.txt");
+
+    // Create the root object
+    auto root = std::make_shared<Object>("Root", Vector3{ 0.0f, 0.0f, 0.0f });
+
+    // For the info in objectData, create the objects
+    for (const auto& objectInfo : objectData) {
+        // Extract object properties from the vector
+        std::string name = objectInfo[0];
+        Vector3 location = { std::stof(objectInfo[1]), std::stof(objectInfo[2]), std::stof(objectInfo[3]) };
+        Vector3 rotation = { std::stof(objectInfo[4]), std::stof(objectInfo[5]), std::stof(objectInfo[6]) };
+        Vector3 scale = { std::stof(objectInfo[7]), std::stof(objectInfo[8]), std::stof(objectInfo[9]) };
+        std::string meshFilepath = "Assets/Scenes/Scene_1/" + name + ".glb";
+
+        // Create the object
+        auto object = std::make_unique<MeshInstance>(name, location, meshFilepath);
+        // Add object to tree
+        root->addChild(std::move(object));        
+    }
 
     // Ready
     root->ready();
